eef226fa8d26c37c826c8e81e8ca96edb3534b40
Artur Gajewski <artur.gajewski@fraktio.fi>
2014-03-07 09:31:59 +0200

    Added updates entity and view

---
+++ src/Blog/MainBundle/Service/UpdateService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Blog\MainBundle\Service;
+
+use Blog\MainBundle\Entity\Update;
+
+use Blog\MainBundle\Entity\UpdateRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Security\Core\SecurityContext;
+
+use Exception;
+
+class UpdateService
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var UpdateRepository
+     */
+    protected $repository;
+
+    /**
+     * @var SecurityContext
+     */
+    protected $security;
+
+    /**
+     * @param EntityManager     $em
+     * @param UpdateRepository  $repository
+     * @param SecurityContext   $security
+     */
+    public function __construct(
+        EntityManager       $em,
+        UpdateRepository  $repository,
+        SecurityContext     $security)
+    {
+        $this->em         = $em;
+        $this->repository = $repository;
+        $this->security   = $security;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUpdates()
+    {
+        $updates = $this->repository->findAll();
+
+        return $updates ? $updates[0] : null;
+    }
+
+    /**
+     * Save an update
+     *
+     * @param  Update $update
+     * @return Update
+     */
+    public function saveUpdate(Update $update)
+    {
+        $this->em->persist($update);
+        $this->em->flush();
+
+        return $update;
+    }
+
+}
